b7878f0a9a855db62f44f5699e1df228577112b4
anusha216 <k.anusha@suntechnoservices.com>
2020-03-03 13:42:42 +0530

    io commit

---
+++ src/test/java/com/example/springioemp/SpringioempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springioemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringioempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
